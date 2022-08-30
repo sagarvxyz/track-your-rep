3bff2340af767607658c9548448ee27cd9e0e513
sagarvxyz <sagar.velagala@gmail.com>
2022-08-30 14:18:02 -0400

    chore: renamed folder src -> ui

--- ui/components/app.jsx
+++ ui/components/app.jsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
+import { VotingForm } from './votingForm'
 
 const App = (props) => {
   return(
     <Fragment>
       <h1>Track Your Rep</h1>
       <p>Viva la revolucion!</p>
+      <VotingForm/>
     </Fragment>
   )
 }
---
+++ ui/components/votingForm.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+const VotingForm = (props) => {
+  return(
+    <Fragment>
+      <button>Yay</button>
+      <button>Nay</button>
+      <button>Abstain</button>
+    </Fragment>
+  )
+}
+export { VotingForm }
